9bf96d5b24ac64228e6f1cf02fa330276c47a526
MATHEUS MACIEL DA SILVA <a95827731@rn.senac.local>
2025-10-23 20:48:49 -0300

    atual

--- aula1/main.js
+++ aula1/main.js
@@ -18,3 +18,26 @@ app.whenReady().then(() => {
     .catch((erro) => {
         console.log(erro)
     })
+
+    //quet3
+
+    const temp = () => {
+        let continua = 'S'
+
+        while(continua = 'S'){
+            const temperatura = prompt('Qual temperatura você quer converter? (K ou F): ').toUpperCase
+            const celcius = Number(prompt('Digite a temperatura em celcius: '))
+
+            if(temperatura === 'K'){
+                const kelvin = celcius + 273.15
+                console.log(`Temperatura em kelvin: ${kelvin}K`)
+            }else if(temperatura === 'F'){
+                const fahrenheit = (celcius * 9/5) + 32
+                console.log(`Temperatura em fahrenheit é: ${fahrenheit}F`)
+            }else{
+                console.log('Opção inválida!')
+            }
+            continua = prompt('Deseja converter novamente? ')
+        }
+        console.log('Finalizado!')
+    }
