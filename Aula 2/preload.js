e7d6885ecc49d4c6bf06f792f96b6f5277650e25
MatheusMS007 <matheusmasiel@gmail.com>
2025-11-17 19:27:39 -0300

    Delete Aula 2 directory

--- Aula 2/preload.js
+++
@@ -1,6 +0,0 @@
-import { contextBridge, ipcRenderer} from 'electron'
-
-contextBridge.exposeInMainWorld("api", {
-  fazerLogin: () => ipcRenderer.send("login"),
-  receberResultado: (callback) => ipcRenderer.on("login-resposta", (event, data) => callback(data))
-});
